81bfef46fe08ce5d81099e9395526ab28afd77b2
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-08 01:48:05 -0300

    update favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Head from "next/head";
 import { About } from "@/components/About";
 import { Header } from "@/components/Header";
 import { Experience } from "@/components/Experience";
@@ -7,12 +8,21 @@ import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col xl:w-[60%] justify-center mx-auto xl:p-8">
-      <Header />
-      <About />
-      <Experience />
-      <Education />
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="https://github.com/solidsnk86/CvOnline-modelo1/blob/master/img/cv.png?raw=true"
+          type="image/x-icon"
+        />
+      </Head>
+      <main className="flex flex-col xl:w-[60%] justify-center mx-auto xl:p-8">
+        <Header />
+        <About />
+        <Experience />
+        <Education />
+        <Footer />
+      </main>
+    </>
   );
 }
