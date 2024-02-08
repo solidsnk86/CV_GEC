789f22efc6f3528586c198a24be05cf74247f8d7
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-07 23:07:24 -0300

    update icons w - h

--- src/components/icons/Worldmap.tsx
+++ src/components/icons/Worldmap.tsx
@@ -2,8 +2,8 @@ export default function WorldMap() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="12"
-      height="12"
+      width="13"
+      height="13"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
