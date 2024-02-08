366bd836c53f12335f934640ea6984700b882ed1
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-08 06:43:56 -0300

    upp

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,12 +9,12 @@ export const Skills = () => {
   const { data } = useData(columnName, CSV_URL_APTITUDES);
 
   return (
-    <Section className="grid xl:gap-4 gap-1">
+    <Section className="grid gap-4">
       <SectionTitle title="Aptitudes" />
-      <div className="grid grid-cols-5 my-4">
+      <div className="grid xl:grid-cols-5 grid-cols-2">
         {data.map((D) => (
           <article key={D.APTITUDES}>
-            <p className="bg-[#EEEEEE] border border-zinc-200 px-2 py-1 rounded-md xl:text-xs text-[10px] text-zinc-800 m-1 w-fit">
+            <p className="bg-[#EEEEEE] border border-zinc-200 px-2 py-1 rounded xl:text-xs text-[10px] text-zinc-800 m-1 w-fit">
               {D.APTITUDES}
             </p>
           </article>
