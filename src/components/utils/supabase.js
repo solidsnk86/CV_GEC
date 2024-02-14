f81e1c39ffd5d97130f5d47fe877c1819427eea0
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-14 11:00:24 -0300

    update

--- src/components/utils/supabase.js
+++ src/components/utils/supabase.js
@@ -5,8 +5,4 @@ const SUPABASE_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
-supabase.storage.config = {
-  url: "https://supabase.com/dashboard/project/yyqjcfzddjozcwahhugs/storage/buckets/simbiosis",
-};
-
 export default supabase;
