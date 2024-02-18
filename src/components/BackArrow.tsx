2a7529cb8b16678ea655722c954defa9a0ba5c7d
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-02-18 06:01:04 -0300

    .

--- src/components/BackArrow.tsx
+++ src/components/BackArrow.tsx
@@ -2,10 +2,12 @@ import { ArrowLeft } from "lucide-react";
 
 export const BackArrow = ({ className = "" }) => {
     return (
-        <div title="Volver al Inicio" className="cursor-pointer hover:bg-[#eee] p-1 w-fit rounded-md xl:my-4 xl:mx-3 border text-zinc-500" onClick={() => window.open('/')}>
-            <ArrowLeft
-                className={`w-6 ${className}`}
-            />
+        <div
+            title="Volver al Inicio"
+            className="cursor-pointer hover:bg-[#eee] p-1 w-fit rounded-md xl:my-4 xl:mx-3 border text-zinc-500"
+            onClick={() => window.open("/")}
+        >
+            <ArrowLeft className={`w-6 ${className}`} />
         </div>
     );
 };
